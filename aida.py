50911488ff5d81f66f024faec32d543ecdda8265
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2022-04-08 19:21:58 +0100

    Update aida.py
    Added loads of new functions: cpu,gpu,img,filename,slug,zip,uzip

--- aida.py
+++ aida.py
@@ -1,5 +1,6 @@
+import cv2
 import os
-import shutil
+import os.path
 from google.colab import drive
 from dirsync import sync
 from rich.console import Console
@@ -7,6 +8,9 @@ console = Console()
 from slugify import slugify
 import configparser
 import IPython
+import tarfile
+
+import shutil
 
 def setup(renderer):
     from dirsync import sync
@@ -48,3 +52,47 @@ def lsDir(dir):
 
 def time():
     return [time.strftime("_%H%M%S")]
+
+def zip(filename, source):
+    with tarfile.open(filename, "w:gz") as tar:
+        tar.add(source, arcname=os.path.basename(source))
+
+def uzip(filename, target):
+    my_tar = tarfile.open(filename)
+    my_tar.extract(target)
+    my_tar.close()
+
+# */ Clean -> Get file as slug *\
+def slug(text):
+    t = slugify(text, entities=True, decimal=True, hexadecimal=True, max_length=0, word_boundary=False, separator='_',
+                save_order=False, stopwords=(), regex_pattern=None, lowercase=True, replacements=(),
+                allow_unicode=False)
+    return t
+
+# */ Clean -> Get files without path *\
+def filename(file):
+    parts = file.rpartition('.')
+    if len(parts) > 1:
+        return ''.join(parts[:-1]).rstrip('.') if parts[0] else file
+    else:
+        return file
+    
+def gpu():
+    !nvidia - smi
+
+
+def cpu():
+    info = ["Model name:", "L3 cache:"]
+    for ob in info:
+        !lscpu | grep
+        "$ob"
+    !nvidia - smi
+    
+def img(image, scale):
+    image_show = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+    scale_percent = scale  # percent of original size
+    width_show = int(image_show.shape[1] * scale_percent / 100)
+    height_show = int(image_show.shape[0] * scale_percent / 100)
+    dim = (width_show, height_show)
+    resized = cv2.resize(image_show, dim, interpolation=cv2.INTER_AREA)
+    cv2_imshow(resized)
