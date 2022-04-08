8133af028391f7199e0878a54da2308c51fc53a5
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2022-04-08 15:06:05 +0100

    Add files via upload

---
+++ aida.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+from google.colab import drive
+from dirsync import sync
+from rich.console import Console
+console = Console()
+from slugify import slugify
+import configparser
+import IPython
+
+def setup(renderer):
+    from dirsync import sync
+    localDir = '/content/in'
+    remoteDir = f'/content/drive/MyDrive/aida/renderer/{renderer}/setup'
+    drive.mount('/content/drive')
+    os.makedirs(localDir, exist_ok="True")
+    sync(remoteDir, localDir, 'sync', create=True)
+
+def txt(action, details):
+    console.print(f"[bright_white]{action}[/bright_white] -> [r black]{details}[/r black]")
+
+def txtM(action, details):
+    console.print(f"[bright_magenta]{action}[/bright_magenta] -> [r black]{details}[/r black]")
+
+def txtC(action, details):
+    console.print(f"[bright_cyan]{action}[/bright_cyan] -> [r black]{details}[/r black]")
+
+def txtY(action, details):
+    console.print(f"[bright_yellow]{action}[/bright_yellow] -> [r black]{details}[/r black]")
+
+def txtB(action, details):
+    console.print(f"[bright_black]{action}[/bright_black] -> [r black]{details}[/r black]")
+
+def mount():
+    drive.mount('/content/drive')
+
+def syncDir(source, target):  
+    sync(source, target, 'sync', create=True)
+
+def rm(dir):
+    shutil.rmtree(dir)
+
+def mk(dir):
+    os.makedirs(dir, exist_ok="True")
+    
+def lsDir(dir):
+    return [os.path.join(dir, file) for file in os.listdir(dir)]
+
+def time():
+    return [time.strftime("_%H%M%S")]
